928597fb6b981ae48090c5d932495bba4676fd3e
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-12-14 03:23:18 +0000

    feat: Add vite-aliases, change imports with aliases

---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+export { default as ButtonNetwork } from './Buttons/ButtonNetwork';
+export { default as CardAuthor } from './Cards/CardAuthor';
+export { default as CardGenerate } from './Cards/CardGenerate';
+export { default as Home } from './Home';
+export { default as RenderConditional } from "./RenderConditional/renderConditonal";
+export { default as Spinner } from './Spinners/Spinner';
---
+++ src/Function/index.js
@@ -0,0 +1,4 @@
+export { default as generateImage } from './generateImage';
+export { default as generateUrl } from './generateUrl';
+export { default as getRandomColor } from './getRandomColor';
+export { default as shareTwitter } from './shareTwitter';
